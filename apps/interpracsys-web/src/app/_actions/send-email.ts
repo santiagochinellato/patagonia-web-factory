930e5066823f030c2e3fb75499c33cc68fcf97da
santiagochinellato <santiagochinellato@gmail.com>
2025-12-23 17:31:53 +0000

    add fixed

--- apps/interpracsys-web/src/app/_actions/send-email.ts
+++ apps/interpracsys-web/src/app/_actions/send-email.ts
@@ -16,7 +16,7 @@ export async function sendEmail(prevState: any, formData: FormData) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: false, // true for 465, false for other ports
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
